c0b9eacca214afb495b2be26337ceaa1b70cc799
Ryo Manzoku <manzoku.ryo@gmail.com>
2019-04-10 19:25:09 +0900

    meta transaction

--- contracts/MetaMarking.sol
+++ contracts/MetaMarking.sol
@@ -1,29 +1,72 @@
 pragma solidity 0.5.7;
 
-contract MCHMetaMarking {
+import "./lib/github.com/OpenZeppelin/openzeppelin-solidity-2.2.0/contracts/cryptography/ECDSA.sol";
+import "./lib/github.com/doublejumptokyo/contract-library-0.0.4/contracts/access/roles/OperatorRole.sol";
 
+contract MCHMetaMarking is OperatorRole {
+
+  mapping(address => uint256) public nonces;
+  mapping(address => bool) public relayers;
   struct Mark {
     bool isExist;
     uint markAt;
-    // uint32 uid;
+    uint32 uid;
+    bool isPrime;
     uint8 landType;
-    // bool isPrime;
   }
-
 
   mapping(uint8 => address[]) public addressesByLandType;
   mapping(address => Mark) public latestMarkByAddress;
 
-
   constructor() public {
+    addOperator(address(0xd868711BD9a2C6F1548F5f4737f71DA67d821090));
+    relayers[address(0xd868711BD9a2C6F1548F5f4737f71DA67d821090)] = true;
   }
 
-  function mark(uint8 _landType, address _user) public {
+  function setRelayer(address _relayer, bool _new) external onlyOwner() {
+    relayers[_relayer] = _new;
+  }
+
+  function encodeData(address _from, uint256 _markAt, uint8 _landType, uint256 _nonce, address _relayer) public view returns (bytes32) {
+    return keccak256(abi.encodePacked(
+                                      address(this),
+                                      _from,
+                                      _markAt,
+                                      _landType,
+                                      _nonce,
+                                      _relayer
+                                      )
+                     );
+  }
+
+  function ecdsaRecover(bytes32 _data, bytes memory _sig) public pure returns (address) {
+    bytes32 data = ECDSA.toEthSignedMessageHash(_data);
+    return ECDSA.recover(data, _sig);
+  }
+
+  function executeMarkMetaTx(address _from, uint256 _markAt, uint32 _uid, bool _isPrime, uint8 _landType, uint256 _nonce, bytes calldata _sig) external {
+    require(relayers[msg.sender] == true, "msg.sender is not relayer");
+    require(nonces[_from]+1 == _nonce, "nonces[_from]+1 != _nonce");
+    bytes32 encodedData = encodeData(_from, _markAt, _landType, _nonce, msg.sender);
+    address signer = ecdsaRecover(encodedData, _sig);
+    require(_from == signer, "signer != _from");
+
+    _mark(_from, _markAt, _uid, _isPrime, _landType);
+    nonces[_from]++;
+  }
+
+  function forceMark(address _user, uint256 _markAt, uint32 _uid, bool _isPrime, uint8 _landType) external onlyOperator() {
+    _mark(_user, _markAt, _uid, _isPrime, _landType);
+  }
+
+  function _mark(address _user, uint256 _markAt, uint32 _uid, bool _isPrime, uint8 _landType) private {
 
     if (!latestMarkByAddress[_user].isExist) {
       latestMarkByAddress[_user] = Mark(
                                         true,
-                                        block.number,
+                                        _markAt,
+                                        _uid,
+                                        _isPrime,
                                         _landType
                                         );
       addressesByLandType[_landType].push(_user);
@@ -43,38 +86,53 @@ contract MCHMetaMarking {
       addressesByLandType[_landType].push(_user);
     }
 
-    latestMarkByAddress[_user].markAt = block.number;
+    latestMarkByAddress[_user].markAt = _markAt;
+    latestMarkByAddress[_user].uid = _uid;
+    latestMarkByAddress[_user].isPrime = _isPrime;
     latestMarkByAddress[_user].landType = _landType;
   }
 
-  function getAddressesByLandType(uint8 _landType) public view returns (address[] memory){
+  function getAddressesByLandType(uint8 _landType, uint256 _validBlockFrom) public view returns (address[] memory){
     if (addressesByLandType[_landType].length == 0) {
       return new address[](0);
     }
 
     uint256 cnt;
     for (uint256 i = 0; i < addressesByLandType[_landType].length; i++) {
-      if (addressesByLandType[_landType][i] == address(0x0)) {
-	continue;
+      address addr = addressesByLandType[_landType][i];
+      if (addr == address(0x0)) {
+        continue;
       }
-      cnt++;
+
+      if (latestMarkByAddress[addr].markAt >= _validBlockFrom) {
+        cnt++;
+      }
     }
 
     address[] memory ret = new address[](cnt);
     uint256 idx = 0;
     for (uint256 i = 0; i < addressesByLandType[_landType].length; i++) {
-      if (addressesByLandType[_landType][i] == address(0x0)) {
-	continue;
+      address addr = addressesByLandType[_landType][i];
+      if (addr == address(0x0)) {
+        if (latestMarkByAddress[addr].markAt < _validBlockFrom) {
+          continue;
+        }
+
+        if (latestMarkByAddress[addr].markAt >= _validBlockFrom) {
+          ret[idx] = addressesByLandType[_landType][i];
+          idx++;
+        }
       }
-      ret[idx] = addressesByLandType[_landType][i];
-      idx++;
     }
 
     return ret;
   }
 
-  function kill() external {
-    selfdestruct(msg.sender);
+  function meta_nonce(address _from) external view returns (uint256 nonce) {
+    return nonces[_from];
   }
 
+  function kill() external onlyOperator() {
+    selfdestruct(msg.sender);
+  }
 }
